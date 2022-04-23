f92d25872eb7b66ffbd5c83ba9da23a38ea2d971
Danil <danilsmy.edu@gmail.com>
2022-04-24 02:17:34 +0300

    File redo

--- LINQToObjects/LINQToObjects/Program.cs
+++ LINQToObjects/LINQToObjects/Program.cs
@@ -23,7 +23,7 @@ namespace LINQToObjects
             Project project1 = new Project("123-ASO", "1.Formula Prom", 23400,
                 new DateTime(2016, 5, 28),
                 new DateTime(2023, 3, 17),
-                new List<Person> 
+                new List<Person>
                 {
                     person1, person2, person3
                 });
@@ -74,19 +74,18 @@ namespace LINQToObjects
             };
 
             // CREATE FACTORIES
-            Factory factory1 = new Factory("1.Wool Products",new List<Project> { project1,project3 });
-            Factory factory2 = new Factory("2.Hanry Laundries",new List<Project> { project2 });
-            Factory factory3 = new Factory("3.Mekha Weapons",new List<Project> { project4 });
+            Factory factory1 = new Factory("1.Wool Products", new List<Project> { project1, project3 });
+            Factory factory2 = new Factory("2.Hanry Laundries", new List<Project> { project2 });
+            Factory factory3 = new Factory("3.Mekha Weapons", new List<Project> { project4 });
             project1.factory = factory1;
             project3.factory = factory1;
             project2.factory = factory2;
             project4.factory = factory3;
             List<Factory> factories = new List<Factory> { factory1, factory2, factory3 };
-            List<Person> people = new List<Person> { person1,person2,person3,person4,person5};
-            List<Project> projects = new List<Project> { project1,project2,project3,project4 };
+            List<Person> people = new List<Person> { person1, person2, person3, person4, person5 };
+            List<Project> projects = new List<Project> { project1, project2, project3, project4 };
 
             // 1. Participants in project
-<<<<<<< HEAD
             Console.WriteLine("*****1. Participants in project");
 
             var q1 = project1.participants.Select(p => p);
@@ -119,64 +118,28 @@ namespace LINQToObjects
             // 6. Ordered by ascending of number of their projcets and surnames people
             // who are involved in more than 1 project 
             Console.WriteLine("*****6. Ordered by ascending of number of their projcets and surnames people who are involved in more than 1 project");
-=======
-            var q1 = project1.participants.Select(p => p);
-            //print(q1);
-
-            // 2. Projects names in factory
-            var q2 = factory1.projects.Select(p => p.name);
-            //print(q2);
-
-            // 3. Person's project codes and names
-            var q3 = person3.participateIn.Select(p => new { p.code, p.name });
-            //print(q3);
-
-            // 4. Factories with more than 1 project 
-            var q4 = factories.Where(f => f.projects.Count > 1);
-            //print(q4);
-
-            // 5. People ordered by ascending of their surnames
-            var q5 = people.OrderBy(p => p.surname);
-            //print(q5);
-
-            // 6. Ordered by ascending of number of their projcets and surnames people
-            // who are involved in more than 1 project 
->>>>>>> main
 
             var q6 = people
                 .Where(p => p.participateIn.Count > 1)
                 .OrderBy(p => p.participateIn.Count)
                 .ThenBy(p => p.surname);
-<<<<<<< HEAD
             print(q6);
 
             // 7. Name and cost of top 1 cost project
             Console.WriteLine("*****7. Name and cost of top 1 cost project");
-=======
-            //print(q6);
-
-            // 7. Name and cost of top 1 cost project
->>>>>>> main
 
             var q7 = projects
                 .OrderByDescending(p => p.cost)
                 .Select(p => new { p.name, p.cost })
                 .First();
-<<<<<<< HEAD
             Console.WriteLine(q7);
 
             // 8. Factories join projects 
             Console.WriteLine("*****8. Factories join projects");
-=======
-            //Console.Write(q7);
-
-            // 8. Factories join projects 
->>>>>>> main
 
             var q8 = factories
-                .Join(projects, f => f, p => p.factory, 
-                (f,p) => new { Factory = f.name, Project = p.name});
-<<<<<<< HEAD
+                .Join(projects, f => f, p => p.factory,
+                (f, p) => new { Factory = f.name, Project = p.name });
             print(q8);
 
             // 9. Projects ordered by ascending of execution time
@@ -185,7 +148,7 @@ namespace LINQToObjects
             var q9 = from p in projects
                      let time = (p.endTime.Subtract(p.startTime)).Days
                      orderby time
-                     select new {projectName = p.name,timeInDays = time};
+                     select new { projectName = p.name, timeInDays = time };
             print(q9);
 
             // 10. Project group by factory they are being execute on
@@ -229,7 +192,7 @@ namespace LINQToObjects
 
             var q14 = from p in projects
                       group p by p.startTime.Year into temp
-                      select new {Year = temp.Key, Count = temp.Count()};
+                      select new { Year = temp.Key, Count = temp.Count() };
             print(q14);
 
             // 15. People that are under 25 and over 40 y.o.
@@ -242,13 +205,6 @@ namespace LINQToObjects
                        where p.age < 25
                        select p);
             print(q15);
-=======
-            //print(q8);
-
-            // 9. Projects ordered by ascending of execution time
-
-
->>>>>>> main
 
         }
         public static void print<T>(IEnumerable<T> lst)
